3c9b45b6a6169ce507aef0916fcab8444f80d300
Aidan Cully <github@aidan.users.panix.com>
2015-01-28 09:35:42 -0500

    new API is more future-proof, and more internally consistent.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 // offsetof()-like operation. Will become obsolete when-and-if offsetof() is
 // implemented in the core language.
 #[macro_export]    
-macro_rules! field_offset {
-    // FIXME: I'm having a hard time figuring out how to make this
-    // hygienic.
+macro_rules! containerof_field_offset {
     ($container:ty : $field:ident) => (unsafe {
         let nullptr = 0 as * const $container;
         let fieldptr: * const _ = &((*nullptr).$field);
@@ -13,56 +11,122 @@ macro_rules! field_offset {
     })
 }
 
-#[macro_export]    
-macro_rules! intrusive {
+#[macro_export]
+macro_rules! containerof_intrusive {
     ($nt:ident = $container:ty : $field:ident :: $fieldtype:ty) => (
         // FIXME: $nt should really be a linear type (it is an error to
         // drop an instance of $nt, as the container needs to be recovered
         // for drop to succeed, so drops should be prevented at
-        // compiler-level), but Rust yet doesn't support linear types.
+        // compiler-level), but Rust doesn't yet support linear types.
         struct $nt(usize);
         impl ::containerof::Intrusive for $nt {
             type Container = $container;
             type Field = $fieldtype;
 
             #[inline]
-            fn move_from(c: Box<$container>) -> Self {
-                unsafe {
-                    let cp: *const $container = ::std::mem::transmute(c);
-                    $nt(::std::mem::transmute(&((*cp).$field)))
-                }
+            unsafe fn from_container(c: Box<$container>) -> Self {
+                let cp: *const $container = ::std::mem::transmute(c);
+                $nt(::std::mem::transmute(&((*cp).$field)))
             }
             #[inline]
-            fn container_of(self) -> Box<$container> {
+            unsafe fn into_container(self) -> Box<$container> {
+                let fieldptr = self.0;
+                let containerptr = fieldptr - containerof_field_offset!($container:$field);
+                ::std::mem::transmute(containerptr)
+            }
+            #[inline]
+            fn as_container<'a>(&'a self) -> &'a $container {
                 unsafe {
-                    let fieldptr: usize = ::std::mem::transmute(self.0);
-                    let containerptr =
-                        fieldptr - field_offset!($container:$field);
+                    let fieldptr = self.0;
+                    let containerptr = fieldptr - containerof_field_offset!($container:$field);
                     ::std::mem::transmute(containerptr)
                 }
             }
             #[inline]
-            fn as_field(&self) -> &$fieldtype {
-                unsafe {
-                    ::std::mem::transmute(self.0)
-                }
+            fn as_container_mut<'a>(&'a mut self) -> &'a mut $container {
+                unsafe { ::std::mem::transmute(self.as_container()) }
+            }
+
+            #[inline]
+            unsafe fn from_field(c: Box<$fieldtype>) -> Self {
+                $nt(::std::mem::transmute(c))
             }
             #[inline]
-            fn as_field_mut(&mut self) -> &mut $fieldtype {
-                unsafe {
-                    ::std::mem::transmute(self.0)
-                }
+            unsafe fn into_field(self) -> Box<$fieldtype> {
+                ::std::mem::transmute(self.0)
+            }
+            #[inline]
+            fn as_field<'a>(&'a self) -> &'a $fieldtype {
+                unsafe { ::std::mem::transmute(self.0) }
+            }
+            #[inline]
+            fn as_field_mut<'a>(&'a mut self) -> &'a mut $fieldtype {
+                unsafe { ::std::mem::transmute(self.0) }
+            }
+
+            unsafe fn from_alias(ia: IntrusiveAlias) -> Self {
+                ::std::mem::transmute(ia)
+            }
+            unsafe fn into_alias(self) -> IntrusiveAlias {
+                ::std::mem::transmute(self)
+            }
+            unsafe fn as_alias<'a>(&'a self) -> &'a IntrusiveAlias {
+                ::std::mem::transmute(self)
+            }
+            unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut IntrusiveAlias {
+                ::std::mem::transmute(self)
+            }
+            unsafe fn of_alias<'a>(ia: &'a IntrusiveAlias) -> &'a Self {
+                ::std::mem::transmute(ia)
+            }
+            unsafe fn of_alias_mut<'a>(ia: &'a mut IntrusiveAlias) -> &'a mut Self {
+                ::std::mem::transmute(ia)
             }
         }
     )
 }
 
+// Defined in the same way as the `newtype` formed by the intrusive
+// macro, *should* have the same representation. (FIXME: can this be
+// guaranteed, somehow? Right now, we rely on std::mem::transmute's
+// promise not to compile when provided an incompatible data type, but
+// I'm still nervous about this.) Intended to be provided as an input
+// to `Intrusive`'s `*_alias` routines, making it easier for
+// implementors of intrusive structures to use common implementations.
+#[derive(PartialEq,Eq,Copy,Clone)]
+pub struct IntrusiveAlias(pub usize);
+
 pub trait Intrusive {
     type Container;
     type Field;
 
-    fn move_from(Box<Self::Container>) -> Self;
-    fn container_of(self) -> Box<Self::Container>;
-    fn as_field(&self) -> &Self::Field;
-    fn as_field_mut(&mut self) -> &mut Self::Field;
+    // FIXME: I'm not sure that "Box" is correct to use for these
+    // "from/to" APIs. Idea is to represent the ownership in a
+    // pointer, but does dropping a Box<> pointer generally cause a
+    // `free` operation? If so, using these APIs may cause the wrong
+    // thing to be done, if the box was not initially obtained via a
+    // `malloc`.
+    //
+    // Since I'm not sure what the correct thing to do is, mark the
+    // `ownership` transferring functions as `unsafe`, so that users
+    // will know that places where these functions are called will
+    // need extra review to ensure correctness. If I get a better idea
+    // later, we may be able to make safe versions of these routines.
+
+    unsafe fn from_container(Box<Self::Container>) -> Self;
+    unsafe fn into_container(self) -> Box<Self::Container>;
+    fn as_container<'a>(&'a self) -> &'a Self::Container;
+    fn as_container_mut<'a>(&'a mut self) -> &'a mut Self::Container;
+
+    unsafe fn from_field(Box<Self::Field>) -> Self;
+    unsafe fn into_field(self) -> Box<Self::Field>;
+    fn as_field<'a>(&'a self) -> &'a Self::Field;
+    fn as_field_mut<'a>(&'a mut self) -> &'a mut Self::Field;
+
+    unsafe fn from_alias(IntrusiveAlias) -> Self;
+    unsafe fn into_alias(self) -> IntrusiveAlias;
+    unsafe fn as_alias<'a>(&'a self) -> &'a IntrusiveAlias;
+    unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut IntrusiveAlias;
+    unsafe fn of_alias<'a>(&'a IntrusiveAlias) -> &'a Self;
+    unsafe fn of_alias_mut<'a>(&'a mut IntrusiveAlias) -> &'a mut Self;
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,5 @@
 #![cfg(test)]
-#[macro_use(field_offset, intrusive)]
+#[macro_use]
 extern crate containerof;
 use containerof::*;
 
@@ -10,79 +10,111 @@ struct MyStruct {
     field3: i32,
 }
 
-impl MyStruct {
-    pub fn field1_offset() -> usize {
-        field_offset!(MyStruct:field1)
-    }
-    pub fn field2_offset() -> usize {
-        field_offset!(MyStruct:field2)
-    }
-    pub fn field3_offset() -> usize {
-        field_offset!(MyStruct:field3)
-    }
-}
-
 struct MyStructField2_Meth1(usize);
-impl ::containerof::Intrusive for MyStructField2_Meth1 {
+impl Intrusive for MyStructField2_Meth1 {
     type Container = MyStruct;
     type Field = i32;
 
-    fn move_from(c: Box<MyStruct>) -> Self {
-        unsafe {
-            let cp: *const MyStruct = ::std::mem::transmute(c);
-            MyStructField2_Meth1(::std::mem::transmute(&((*cp).field2)))
-        }
+    unsafe fn from_container(c: Box<MyStruct>) -> Self {
+        let cp: *const MyStruct = ::std::mem::transmute(c);
+        MyStructField2_Meth1(::std::mem::transmute(&((*cp).field2)))
     }
-    fn container_of(self) -> Box<MyStruct> {
+    unsafe fn into_container(self) -> Box<MyStruct> {
+        let fieldptr = self.0;
+        let containerptr = fieldptr - containerof_field_offset!(MyStruct:field2);
+        ::std::mem::transmute(containerptr)
+    }
+    fn as_container<'a>(&'a self) -> &'a MyStruct {
         unsafe {
             let fieldptr = self.0;
-            let containerptr = fieldptr - field_offset!(MyStruct:field2);
+            let containerptr = fieldptr - containerof_field_offset!(MyStruct:field2);
             ::std::mem::transmute(containerptr)
         }
     }
+    fn as_container_mut<'a>(&'a mut self) -> &'a mut MyStruct {
+        unsafe { ::std::mem::transmute(self.as_container()) }
+    }
+    unsafe fn from_field(c: Box<i32>) -> Self {
+        MyStructField2_Meth1(::std::mem::transmute(c))
+    }
+    unsafe fn into_field(self) -> Box<i32> {
+        ::std::mem::transmute(self.0)
+    }
     fn as_field(&self) -> &i32 {
-        unsafe {
-            ::std::mem::transmute(self.0)
-        }
+        unsafe { ::std::mem::transmute(self.0) }
     }
     fn as_field_mut(&mut self) -> &mut i32 {
-        unsafe {
-            ::std::mem::transmute(self.0)
-        }
+        unsafe { ::std::mem::transmute(self.0) }
+    }
+
+    unsafe fn from_alias(ia: IntrusiveAlias) -> Self {
+        ::std::mem::transmute(ia)
+    }
+    unsafe fn into_alias(self) -> IntrusiveAlias {
+        ::std::mem::transmute(self)
+    }
+    unsafe fn as_alias<'a>(&'a self) -> &'a IntrusiveAlias {
+        ::std::mem::transmute(self)
+    }
+    unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut IntrusiveAlias {
+        ::std::mem::transmute(self)
+    }
+    unsafe fn of_alias<'a>(ia: &'a IntrusiveAlias) -> &'a Self {
+        ::std::mem::transmute(ia)
+    }
+    unsafe fn of_alias_mut<'a>(ia: &'a mut IntrusiveAlias) -> &'a mut Self {
+        ::std::mem::transmute(ia)
     }
 }
-intrusive!(MyStructField2_Meth2 = MyStruct:field2::i32);
+containerof_intrusive!(MyStructField2_Meth2 = MyStruct:field2::i32);
 
 #[test]
 fn test_field_offset() {
-    let offset_field1_meth1 = unsafe {
-        let nullptr = 0 as * const MyStruct;
-        let fieldptr: * const _ = &((*nullptr).field1);
-        fieldptr as usize
+    let ms = MyStruct { field1: 1, field2: 2, field3: 3 };
+    let ms_addr:usize = unsafe { ::std::mem::transmute(&ms) };
+    let ms_field1_addr:usize = unsafe { ::std::mem::transmute(&ms.field1) };
+    let ms_field2_addr:usize = unsafe { ::std::mem::transmute(&ms.field2) };
+    let ms_field3_addr:usize = unsafe { ::std::mem::transmute(&ms.field3) };
+    assert_eq!(ms_field1_addr - ms_addr, containerof_field_offset!(MyStruct:field1));
+    assert_eq!(ms_field2_addr - ms_addr, containerof_field_offset!(MyStruct:field2));
+    assert_eq!(ms_field3_addr - ms_addr, containerof_field_offset!(MyStruct:field3));
+}
+#[test]
+fn test_intrusive_container_roundtrip() {
+    let mc1 = Box::new(MyStruct { field1: 1, field2: 2, field3: 3 });
+    let mc1_addr: usize = unsafe { ::std::mem::transmute(&*mc1) };
+
+    let mcfield: MyStructField2_Meth1 = unsafe { Intrusive::from_container(mc1) };
+    let mcfieldcontainer: usize = unsafe {
+        ::std::mem::transmute(mcfield.as_container())
     };
-    let offset_field1_meth2 = MyStruct::field1_offset();
-    assert_eq!(offset_field1_meth1, offset_field1_meth2);
-    assert_eq!(0, offset_field1_meth2);
-    assert_eq!(4, MyStruct::field2_offset());
-    assert_eq!(8, MyStruct::field3_offset());
-}
-#[test]
-fn test_intrusive_meth1() {
-    let mc1 = Box::new(MyStruct { field1: 1, field2: 2, field3: 3 });
-    let mc1_addr: usize = unsafe { ::std::mem::transmute_copy(&mc1) };
-    let mcfield: MyStructField2_Meth1 = ::containerof::Intrusive::move_from(mc1);
-    let mc2 = mcfield.container_of();
+
+    assert_eq!(mc1_addr, mcfieldcontainer);
+    ::std::mem::drop(mcfieldcontainer);
+
+    let mc2 = unsafe { mcfield.into_container() };
     let mc2_addr: usize = unsafe { ::std::mem::transmute_copy(&mc2) };
     assert_eq!(mc1_addr, mc2_addr);
 }
+
 #[test]
-fn test_intrusive_meth2() {
+fn test_intrusive_field_roundtrip() {
     let mc1 = Box::new(MyStruct { field1: 1, field2: 2, field3: 3 });
-    let mc1_addr: usize = unsafe { ::std::mem::transmute_copy(&mc1) };
-    let mut mcfield: MyStructField2_Meth2 = ::containerof::Intrusive::move_from(mc1);
-    *(mcfield.as_field_mut()) = 10;
-    let mc2 = mcfield.container_of();
-    let mc2_addr: usize = unsafe { ::std::mem::transmute_copy(&mc2) };
+    let mc1_addr: usize = unsafe { ::std::mem::transmute(&*mc1) };
+    let mc1_field_addr: usize = unsafe { ::std::mem::transmute(&mc1.field2) };
+
+    let mcfield: MyStructField2_Meth1 = unsafe { Intrusive::from_container(mc1) };
+    let mcfield_as_addr: usize = unsafe {
+        ::std::mem::transmute(mcfield.as_field())
+    };
+    assert_eq!(mc1_field_addr, mcfield_as_addr);
+
+    let mcfield = unsafe { mcfield.into_field() };
+    let mcfield_addr = unsafe { ::std::mem::transmute(&*mcfield) };
+    assert_eq!(mc1_field_addr, mcfield_addr);
+
+    let mcfield: MyStructField2_Meth1 = unsafe { Intrusive::from_field(mcfield) };
+    let mc2 = unsafe { mcfield.into_container() };
+    let mc2_addr: usize = unsafe { ::std::mem::transmute(&*mc2) };
     assert_eq!(mc1_addr, mc2_addr);
-    assert_eq!(10i32, mc2.field2);
 }

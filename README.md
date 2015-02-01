174dd981b371492e1c313b79cd333c474a3bfc0e
Aidan Cully <github@aidan.users.panix.com>
2015-02-01 11:49:35 -0500

    API is more consistent. added documentation. bumped version to 0.1.0

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# rs-intrusive - Support macros for building intrusive data structures
-in Rust.
+% containerof - Macros supporting intrusive data structures in Rust.
 
 An intrusive structure is a general-purpose structure directly
 embedded within a containing structure, in order to add that
@@ -8,17 +7,21 @@ use an intrusive "link" structure to allow objects to be organized in
 a linked-list:
 
 ```rust
+# #[macro_use]
+extern crate containerof;
 struct Link {
-    next: Option<Link>,
+    next: Option<ContainerLink>,
 }
 struct List {
-    head: Option<Link>,
-    tail: Option<Link>,
+    head: Option<ContainerLink>,
+    tail: Option<ContainerLink>,
 }
 
 struct Container {
     link: Link,
 }
+containerof_intrusive!(ContainerLink = Container:link::Link);
+# fn main() {}
 ```
 
 While this module does not provide a linked-list implementation (for
@@ -26,48 +29,48 @@ separation-of-concerns reasons, I believe a linked-list implementation
 belongs in a separate crate), it does provide some necessary
 abstractions for using intrusive structures:
 
-* The `field_offset!` macro, which identifies the location of a field
-  in a containing structure. This isn't too useful in itself, but is
-  necessary to support:
-* The `intrusive!` macro, which provides a newtype for using
-  "intrusive" fields to work with the container object.
+* The `containerof_field_offset!` macro, which identifies the location
+of a field in a containing structure. This isn't too useful in
+itself, but is necessary to support:
+* The `containerof_intrusive!` macro, which provides a newtype for
+using "intrusive" fields to work with the container object.
 
-## Usage
+# Usage
+
+Here is an example implementation of Church-numerals using an
+intrusive linked-list:
 
 ```rust
-#[macro_use(intrusive)]
-extern crate "intrusive";
+#[macro_use]
+extern crate containerof;
+use containerof::*;
 
-struct Link {
-    next: Option<Link>,
-}
-// FIXME: is this legal? want to enforce constraint that `List` only
-// operates against intrusive `Link`s.
-struct<T> List<T> where
-    T: Intrusive<Link>,
-    T::Field: Link,
-{
-    head: Option<LinkType>,
+struct Church {
+    next: Option<ChurchLink>,
 }
 
-struct Container {
-    link: Link,
-}
-intrusive!(ContainerLink = Container:link::Link);
+containerof_intrusive!(ChurchLink = Church:next::Option<ChurchLink>);
 
-fn create() -> List<ContainerLink> {
-    let x = Box::new(Container { link: None });
-    let link: ContainerLink = intrusive::Intrusive::move_from(x);
-    List<ContainerLink> { head: Some(link) }
+impl Church {
+    fn new() -> Box<Church> {
+        Box::new(Church { next: None })
+    }
+    fn push(next: Box<Church>) -> Box<Church> {
+        Box::new(Church { next: Some(unsafe { Intrusive::from_container(next) }) })
+    }
+    fn pop(self: Box<Church>) -> Option<Box<Church>> {
+        match self.next {
+            None => None,
+            Some(x) => Some(unsafe { x.into_container() }),
+        }
+    }
 }
-fn head(list: List<ContainerLink>) -> Box<Container> {
-    list.head.unwrap().container_of()
-}
+# fn main() {}
 ```
 
-## Contributing
+# Contributing
 
-1. Fork it ( https://github.com/aidancully/rs-intrusive/fork )
+1. Fork it ( https://github.com/aidancully/containerof/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,37 @@
-#![crate_name = "containerof"]
+//! Intrusive structure support in Rust.
+//!
+//! An intrusive structure is a general-purpose structure directly
+//! embedded within a containing structure, in order to add that
+//! general-purpose facility to the container. As an example, one
+//! might use an intrusive "link" structure to allow objects to be
+//! organized in a linked-list:
+//!
+//! # Example
+//!
+//! ```test_harness
+//! # #[macro_use]
+//! # extern crate containerof;
+//! struct Link {
+//!     next: Option<ContainerLink>,
+//! }
+//! struct List {
+//!     head: Option<ContainerLink>,
+//!     tail: Option<ContainerLink>,
+//! }
+//! 
+//! struct Container {
+//!     link: Link,
+//! }
+//! containerof_intrusive!(ContainerLink = Container:link::Link);
+//! ```
 
-// offsetof()-like operation. Will become obsolete when-and-if offsetof() is
-// implemented in the core language.
-#[macro_export]    
+#[crate_id = "containerof"]
+#[crate_type = "lib"]
+
+/// Implement C-like `offsetof` macro in Rust. Will become obsolete
+/// when-and-if offsetof() is implemented in the core language.
+#[macro_export]
+#[unstable = "Will go away if-and-when Rust implements this function directly."]
 macro_rules! containerof_field_offset {
     ($container:ty : $field:ident) => (unsafe {
         let nullptr = 0 as * const $container;
@@ -11,7 +40,10 @@ macro_rules! containerof_field_offset {
     })
 }
 
+/// Define a new type that implements the `Intrusive` trait for a field
+/// within a type.
 #[macro_export]
+#[unstable = "Experimental API."]
 macro_rules! containerof_intrusive {
     ($nt:ident = $container:ty : $field:ident :: $fieldtype:ty) => (
         // FIXME: $nt should really be a linear type (it is an error to
@@ -64,48 +96,58 @@ macro_rules! containerof_intrusive {
                 unsafe { ::std::mem::transmute(self.0) }
             }
 
-            unsafe fn from_alias(ia: IntrusiveAlias) -> Self {
+            unsafe fn from_alias(ia: ::containerof::IntrusiveAlias) -> Self {
                 ::std::mem::transmute(ia)
             }
-            unsafe fn into_alias(self) -> IntrusiveAlias {
+            unsafe fn into_alias(self) -> ::containerof::IntrusiveAlias {
                 ::std::mem::transmute(self)
             }
-            unsafe fn as_alias<'a>(&'a self) -> &'a IntrusiveAlias {
+            unsafe fn as_alias<'a>(&'a self) -> &'a ::containerof::IntrusiveAlias {
                 ::std::mem::transmute(self)
             }
-            unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut IntrusiveAlias {
+            unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut ::containerof::IntrusiveAlias {
                 ::std::mem::transmute(self)
             }
-            unsafe fn of_alias<'a>(ia: &'a IntrusiveAlias) -> &'a Self {
+            unsafe fn of_alias<'a>(ia: &'a ::containerof::IntrusiveAlias) -> &'a Self {
                 ::std::mem::transmute(ia)
             }
-            unsafe fn of_alias_mut<'a>(ia: &'a mut IntrusiveAlias) -> &'a mut Self {
+            unsafe fn of_alias_mut<'a>(ia: &'a mut ::containerof::IntrusiveAlias) -> &'a mut Self {
                 ::std::mem::transmute(ia)
             }
         }
     )
 }
 
-// Defined in the same way as the `newtype` formed by the intrusive
-// macro, *should* have the same representation. (FIXME: can this be
-// guaranteed, somehow? Right now, we rely on std::mem::transmute's
-// promise not to compile when provided an incompatible data type, but
-// I'm still nervous about this.) Intended to be provided as an input
-// to `Intrusive`'s `*_alias` routines, making it easier for
-// implementors of intrusive structures to use common implementations.
+/// Alias that has the same representation as an intrusive type. The
+/// idea is to be able to use this alias for intrusive facility
+/// implementations, by defining the "true" implementation of the
+/// facility to use the single (but type-unsafe) IntrusiveAlias type,
+/// while allowing type-safe wrapper implementations to delegate their
+/// behavior to the implementation function.
 #[derive(PartialEq,Eq,Copy,Clone)]
+#[unstable = "Experimental API"]
 pub struct IntrusiveAlias(pub usize);
 
+/// Trait defining routines for translation between containing
+/// structure and intrusive field. The only implementors of this type
+/// should be the pointer-types defined by the `containerof_intrusive`
+/// macro.
+#[unstable = "Experimental API"]
 pub trait Intrusive {
+    /// Type of containing structure.
     type Container;
+    /// Type of intrusive field within containing structure.
     type Field;
+    // TODO: would also like to see an "offset" associated const, but
+    // Rust doesn't seem to support these yet.
 
     // FIXME: I'm not sure that "Box" is correct to use for these
-    // "from/to" APIs. Idea is to represent the ownership in a
+    // "from/into" APIs. Idea is to represent the ownership in a
     // pointer, but does dropping a Box<> pointer generally cause a
-    // `free` operation? If so, using these APIs may cause the wrong
+    // `free` operation? If so, using these APIs will cause the wrong
     // thing to be done, if the box was not initially obtained via a
-    // `malloc`.
+    // `malloc`. What I'd really like is an additional LinearBox<> for
+    // the `into_field` operation.
     //
     // Since I'm not sure what the correct thing to do is, mark the
     // `ownership` transferring functions as `unsafe`, so that users
@@ -113,20 +155,59 @@ pub trait Intrusive {
     // need extra review to ensure correctness. If I get a better idea
     // later, we may be able to make safe versions of these routines.
 
+    /// Represent ownership of a container as ownership of an Intrusive
+    /// pointer type. (Inverse of `into_container`.)
     unsafe fn from_container(Box<Self::Container>) -> Self;
+
+    /// Represent ownership of an Intrusive pointer type as ownership of
+    /// its container. (Inverse of `from_container`.)
     unsafe fn into_container(self) -> Box<Self::Container>;
+
+    /// Grant referential access to the container of this intrusive
+    /// pointer type.
     fn as_container<'a>(&'a self) -> &'a Self::Container;
+    /// Grant mutable referential access to the container of this
+    /// intrusive pointer type.
     fn as_container_mut<'a>(&'a mut self) -> &'a mut Self::Container;
 
+    /// Assuming the "field" is a field in the container object, take
+    /// ownership of the field as an intrusive pointer, allowing
+    /// eventual translation back to the container. (Inverse of
+    /// `into_field`.)
     unsafe fn from_field(Box<Self::Field>) -> Self;
+    
+    /// Represent ownership of the container object as ownership of
+    /// the intrusive field in the object. (Inverse of `from_field`.)
     unsafe fn into_field(self) -> Box<Self::Field>;
+
+    /// Grant referential access to the intrusive field represented by
+    /// this intrusive pointer.
     fn as_field<'a>(&'a self) -> &'a Self::Field;
+
+    /// Grant mutable referential access to the intrusive field represented by
+    /// this intrusive pointer.
     fn as_field_mut<'a>(&'a mut self) -> &'a mut Self::Field;
 
+    /// Ownership-moving translation from generic intrusive pointer
+    /// alias to type-safe intrusive pointer.
     unsafe fn from_alias(IntrusiveAlias) -> Self;
+
+    /// Ownership-moving translation from type-safe intrusive pointer
+    /// to generic intrusive pointer.
     unsafe fn into_alias(self) -> IntrusiveAlias;
+
+    /// Allow using type-safe intrusive pointer as generic intrusive pointer.
     unsafe fn as_alias<'a>(&'a self) -> &'a IntrusiveAlias;
+
+    /// Allow using type-safe intrusive pointer as mutable generic
+    /// intrusive pointer.
     unsafe fn as_alias_mut<'a>(&'a mut self) -> &'a mut IntrusiveAlias;
+
+    /// Allow using generic intrusive pointer as type-safe intrusive
+    /// pointer.
     unsafe fn of_alias<'a>(&'a IntrusiveAlias) -> &'a Self;
+
+    /// Allow using generic intrusive pointer as mutable type-safe
+    /// intrusive pointer.
     unsafe fn of_alias_mut<'a>(&'a mut IntrusiveAlias) -> &'a mut Self;
 }
